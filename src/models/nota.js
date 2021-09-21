f1bbe106b7dc1047369f098c5a5b6dffa7f8e4dd
Angela de Oliveira <aho.angela@gmail.com>
2021-09-20 21:57:03 -0300

    Corrigindo a seleção da menção no model "notas"

--- src/models/nota.js
+++ src/models/nota.js
@@ -15,7 +15,25 @@ class Nota {
     }
 
     mediaCA() {
-        return "SS";
+        const CA = Math.max(
+            0.4 * this.a1 + 0.6 * this.a2, 
+            0.4 * this.a1 + 0.6 * this.a3, 
+            0.4 * this.a3 + 0.6 * this.a2
+        )
+
+        if (CA >= 9 ){
+            return "SS";
+        } else if (CA < 9 && CA >= 7){
+            return "MS";
+        } else if (CA >= 5 && CA < 7){
+            return "MM";
+        } else if (CA < 5 && CA >= 3){
+            return "MI";
+        } else if (CA < 3 && CA >= 1){
+            return "II"
+        } else {
+            return "SR"
+        }
     }
 }
 
